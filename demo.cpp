d2b5b66b97865149e4d8083c34cc85947792a087
Rohit Kumar Barada <rohitbarada@gmail.com>
2025-02-28 10:59:39 +0530

    cpp

---
+++ demo.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a;
+    cout<<"Enter a number: ";
+    cin>>a;
+    cout<<"The number you entered is: "<<a<<endl;
+    return 0;
+}
